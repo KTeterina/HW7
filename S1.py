3e86412ec6a67b477d1b18fe11322acb9f8d93e9
Evgeniia Adamova <st114255@student.spbu.ru>
2024-09-26 09:42:27 +0000

    Change S3.py S1.py

--- S1.py
+++ S1.py
@@ -1,8 +1,11 @@
-greeting = "Hello, it's Katja" #string
+greeting = "Hello, it's Evgeniia" #string
 print(greeting)
 
 print("Some simple calculations")
 
-a = input("Enter a: ")
-b = input("Enter b: ")
-print("The sum of a and b: ", int(a) + int(b))
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+if a < 5:
+    print("The sub of a and b: ", a - b)
+else:
+    print("The sum of a and b: ", a + b)
